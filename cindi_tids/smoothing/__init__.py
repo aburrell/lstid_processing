f98769ced25af37c3479c421a9013abccec8b56d
Angeline Burrell <aburrell@users.noreply.github.com>
2025-05-07 15:37:13 -0400

    STY: renamed package
    Removed files with old package name.

--- cindi_tids/smoothing/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Full license can be found in License.md
-# ----------------------------------------------------------------------------
-"""Sub-module with smoothing utilities."""
-
-# Import the package modules and top-level classes
-from cindi_tids.smoothing import fill_rout  # noqa F401
-from cindi_tids.smoothing  import filter_rout # noqa F401
