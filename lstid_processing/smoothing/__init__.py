ecc6a77c153620d76342ae963e60cf6cfe8960e3
Angeline Burrell <aburrell@users.noreply.github.com>
2025-05-07 18:38:22 -0400

    STY: separated smooth functions
    Put the smooth functions in their own sub-module.

---
+++ lstid_processing/smoothing/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Full license can be found in License.md
+# ----------------------------------------------------------------------------
+"""Sub-module with smoothing utilities."""
+
+# Import the package modules and top-level classes
+from lstid_processing.smoothing import fill_rout  # noqa F401
+from lstid_processing.smoothing  import filter_rout # noqa F401
