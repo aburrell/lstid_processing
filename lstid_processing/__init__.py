5eff07fd747c0e8ce04294d67ee79015cd4ab8cd
Angeline Burrell <aburrell@users.noreply.github.com>
2025-05-07 18:37:51 -0400

    STY: moved cindi code
    Moved CINDI code to a new location under the new package name.

---
+++ lstid_processing/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Full license can be found in License.md
+# ----------------------------------------------------------------------------
+"""Package to identify and process travelling ionospheric disturbances."""
+
+import logging
+from importlib import metadata
+from importlib import resources
+import os.path as _op
+
+# Define a logger object to allow easier log handling
+logging.raiseExceptions = False
+logger = logging.getLogger('lstid_processing')
+
+# Import the package modules and top-level classes
+from lstid_processing import cindi  # noqa F401
+from lstid_processing import model  # noqa F401
+from lstid_processing import plot_rout  # noqa F401
+from lstid_processing import smoothing  # noqa F401
+
+# Define the global variables
+__version__ = metadata.version('lstid_processing')
+
+# Define the model r_os.path.join(
+model_run_dir =  _op.join(resources.files(__package__), 'model_runs')
