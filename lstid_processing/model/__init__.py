679643d819238530efd9e354bb50e0042fc7ed17
Angeline Burrell <aburrell@users.noreply.github.com>
2025-05-07 18:38:46 -0400

    ENH: added model analysis code
    Added model analysis code.

---
+++ lstid_processing/model/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Full license can be found in License.md
+# ----------------------------------------------------------------------------
+"""Sub-module with model processing utilities."""
+
+# Import the package modules and top-level classes
+from lstid_processing.model import analysis  # noqa F401
+from lstid_processing.model import io  # noqa F401
+from lstid_processing.model import plots # noqa F401
